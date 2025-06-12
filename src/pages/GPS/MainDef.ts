724a326ed5cd2493a58e79ab978587e6f3c891e2
taketori <naoki123453@gmail.com>
2025-06-12 09:17:00 +0900

    探索範囲の修正

--- src/pages/GPS/MainDef.ts
+++ src/pages/GPS/MainDef.ts
@@ -33,7 +33,7 @@ function expand(s: number[], t: number): Result {
   })();
   console.log(sc);
   const tp = (() => {
-    let i = p(sc);
+    let i = p(sc) + 1;
     while (i < s.length - 2) {
       if (lessThan(s.slice(p(sc) + 1), s.slice(i + 1)))
         break;
