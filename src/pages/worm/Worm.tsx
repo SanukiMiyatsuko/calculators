7895ec4795e587a1d0ad05210b977715213636a6
taketori <naoki123453@gmail.com>
2025-06-21 05:29:14 +0900

    リンクを追加

--- src/pages/worm/Worm.tsx
+++ src/pages/worm/Worm.tsx
@@ -14,6 +14,12 @@ const Worm = () => {
           hyoki={new worm}
         />
       </main>
+      <footer className={styles.footer}>
+        <p>
+          <a className={styles.a} href="https://googology.fandom.com/ja/wiki/%E3%83%99%E3%82%AF%E3%83%AC%E3%83%9F%E3%82%B7%E3%82%A7%E3%83%95%E3%81%AE%E8%99%AB" target="_blank" rel="noreferrer">ベクレミシェフの虫 | 巨大数研究 Wiki | Fandom</a> (2025/06/21 閲覧)<br />
+          このページは<a className={styles.a} href="https://creativecommons.org/licenses/by-sa/3.0/legalcode" target="_blank" rel="noreferrer">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>の下に公開されます。
+        </p>
+      </footer>
     </>
   );
 };
