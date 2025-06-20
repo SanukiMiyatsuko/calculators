862dd6c2ebb01ac3bf7740417272f82bf76abbdd
taketori <naoki123453@gmail.com>
2025-06-21 02:00:26 +0900

    footerを追加

--- src/pages/secondOrderSearching/SecondOrderSearching.tsx
+++ src/pages/secondOrderSearching/SecondOrderSearching.tsx
@@ -14,6 +14,12 @@ const SecondOrderSearching = () => {
           hyoki={new secondOrderSearching}
         />
       </main>
+      <footer className={styles.footer}>
+        <p>
+          <a className={styles.a} href="https://googology.fandom.com/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC:Kanrokoti/%E4%B8%8B%E6%9B%B8%E3%81%8D2" target="_blank" rel="noreferrer">ユーザー:Kanrokoti/下書き2 | 巨大数研究 Wiki | Fandom</a> (2025/06/21 閲覧)<br />
+          このページは<a className={styles.a} href="https://creativecommons.org/licenses/by-sa/3.0/legalcode" target="_blank" rel="noreferrer">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>の下に公開されます。
+        </p>
+      </footer>
     </>
   );
 };
